5da744444b490d5cb6cd1cbfe4da2e7c1a824a9a
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-10 10:00:02 +0100

    Atualizar o app.py

--- app.py
+++ app.py
@@ -15,11 +15,41 @@ def summarize_text(text):
     summary = summarizer(parser.document, sentences_count=3)
     return ' '.join([str(sentence) for sentence in summary])
 
-st.title('Text Generation and Summarization')
-prompt = st.text_input('Enter a prompt:')
-if st.button('Generate Text'):
-    text = generate_text(prompt)
-    st.write(text)
-if st.button('Summarize Text'):
-    summary = summarize_text(text)
-    st.write(summary)
+def compress_code(code):
+    # Remove comments
+    tokens = [tok for tok in tokenize.generate_tokens(code.splitlines(True).__next__)]
+    # Combine tokens into chunks of up to 2000 tokens
+    chunks = [tokens[i:i+2000] for i in range(0, len(tokens), 2000)]
+    # Convert chunks back into code strings
+    compressed_code = [''.join([t[1] for t in chunk]) for chunk in chunks]
+    return compressed_code
+
+def main():
+    st.title('My Streamlit App')
+    
+    # Add a selectbox to the sidebar:
+    language = st.sidebar.selectbox(
+        'Select a language',
+        ['English', 'Czech', 'German', 'Spanish', 'French', 'Italian', 'Dutch', 'Polish', 'Portuguese', 'Slovak', 'Swedish']
+    )
+    
+    # Add a textarea to the app:
+    code = st.text_area('Enter some Python code')
+    
+    # Add a button to the app:
+    if st.button('Compress Code'):
+        compressed_code = compress_code(code)
+        st.write(compressed_code)
+    
+    prompt = st.text_input('Enter a prompt:')
+    
+    if st.button('Generate Text'):
+        text = generate_text(prompt)
+        st.write(text)
+        
+        if st.button('Summarize Text'):
+            summary = summarize_text(text)
+            st.write(summary)
+
+if __name__ == '__main__':
+    main()

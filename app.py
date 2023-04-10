b963748994c1b751eaf8facfafaf82676d846be3
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-10 22:42:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,34 @@ import streamlit as st
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
+import pyperclip
+
+def summarize_text(text, language):
+    parser = PlaintextParser.from_string(text, Tokenizer(language))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, 10)
+    return ' '.join([str(sentence) for sentence in summary])
+
+def remove_annotations(code):
+    lines = code.split('\n')
+    lines = [line for line in lines if not line.strip().startswith('#')]
+    return '\n'.join(lines)
+
+st.set_page_config(page_title="AI Prompt Engineering", page_icon=":memo:", layout="wide")
+
+st.sidebar.title("AI Prompt Engineering")
+prompt_type = st.sidebar.selectbox("Select prompt type", ["Text", "Code"])
+
+if prompt_type == "Text":
+    st.header("Text")
+    language = st.selectbox("Select language", ["czech", "dutch", "english", "french", "german", "italian", "portuguese", "romanian", "russian", "slovak", "spanish"])
+    text = st.text_area("Enter text timport nltk
+nltk.download('punkt')
+import streamlit as st
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+import pyperclip
 
 def summarize_text(text, language):
     parser = PlaintextParser.from_string(text, Tokenizer(language))
@@ -28,6 +53,24 @@ if prompt_type == "Text":
     st.header("Text")
     language = st.selectbox("Select language", ["czech", "dutch", "english", "french", "german", "italian", "portuguese", "romanian", "russian", "slovak", "spanish"])
     text = st.text_area("Enter text to summarize")
+    if st.button("Summarize"):
+        summary = summarize_text(text, language)
+        st.write(summary)
+        st.write(f"Token size: {len(summary.split())}")
+
+        if st.button("Copy to Clipboard"):
+            pyperclip.copy(summary)
+
+elif prompt_type == "Code":
+    st.header("Code")
+    code = st.text_area("Enter Python code")
+    if st.button("Clean the code"):
+        cleaned_code = remove_annotations(code)
+        st.code(cleaned_code)
+        st.write(f"Token size: {len(cleaned_code.split())}")
+
+        if st.button("Copy to Clipboard"):
+            pyperclip.copy(cleaned_code)o summarize")
     if st.button("Summarize"):
         summary = summarize_text(text, language)
         st.write(summary)

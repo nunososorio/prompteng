fb8e8b3a07c685cba865d970f4f13ce0836b503e
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-10 10:29:11 +0100

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import tokenize
 import streamlit as st
 from transformers import pipeline
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+import tokenize
 
 def generate_text(prompt):
     generator = pipeline('text-generation', model='gpt2')
@@ -18,7 +18,7 @@ def summarize_text(text):
 
 def compress_code(code):
     # Remove comments
-    tokens = [tok for tok in tokenize.generate_tokens(code.splitlines(True).__next__)]
+    tokens = [tok for tok in tokenize.generate_tokens(iter(code.splitlines(True)).__next__)]
     # Combine tokens into chunks of up to 2000 tokens
     chunks = [tokens[i:i+2000] for i in range(0, len(tokens), 2000)]
     # Convert chunks back into code strings
@@ -27,27 +27,27 @@ def compress_code(code):
 
 def main():
     st.title('My Streamlit App')
-    
+
     # Add a selectbox to the sidebar:
     language = st.sidebar.selectbox(
         'Select a language',
         ['English', 'Czech', 'German', 'Spanish', 'French', 'Italian', 'Dutch', 'Polish', 'Portuguese', 'Slovak', 'Swedish']
     )
-    
+
     # Add a textarea to the app:
     code = st.text_area('Enter some Python code')
-    
+
     # Add a button to the app:
     if st.button('Compress Code'):
         compressed_code = compress_code(code)
         st.write(compressed_code)
-    
+
     prompt = st.text_input('Enter a prompt:')
-    
+
     if st.button('Generate Text'):
         text = generate_text(prompt)
         st.write(text)
-        
+
         if st.button('Summarize Text'):
             summary = summarize_text(text)
             st.write(summary)

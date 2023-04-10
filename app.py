32e7f3773782c08b1bdd31304339bb4baabcd1bc
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-10 23:19:35 +0100

    several sumy methods

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ import streamlit as st
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.edmundson import EdmundsonSummarizer
+from sumy.summarizers.kl import KLSummarizer
+import black
 
-def summarize_text(text, language):
+def summarize_text(text, language, summary_size, summarizer_type):
     parser = PlaintextParser.from_string(text, Tokenizer(language))
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, 10)
+    if summarizer_type == 'LsaSummarizer':
+        summarizer = LsaSummarizer()
+    elif summarizer_type == 'LexRankSummarizer':
+        summarizer = LexRankSummarizer()
+    elif summarizer_type == 'LuhnSummarizer':
+        summarizer = LuhnSummarizer()
+    elif summarizer_type == 'EdmundsonSummarizer':
+        summarizer = EdmundsonSummarizer()
+    elif summarizer_type == 'KLSummarizer':
+        summarizer = KLSummarizer()
+    summary = summarizer(parser.document, summary_size)
     return ' '.join([str(sentence) for sentence in summary])
 
 def remove_annotations(code):
@@ -25,15 +39,20 @@ if prompt_type == "Text":
     st.header("Text")
     language = st.selectbox("Select language", ["czech", "dutch", "english", "french", "german", "italian", "portuguese", "romanian", "russian", "slovak", "spanish"])
     text = st.text_area("Enter text to summarize")
+    summary_size = st.slider('Summary size', 1, 20, 10)
+    summarizer_type = st.selectbox("Select Sumy summarizer type", ["LsaSummarizer", "LexRankSummarizer", "LuhnSummarizer", "EdmundsonSummarizer", "KLSummarizer"])
     if st.button("Summarize"):
-        summary = summarize_text(text, language)
+        summary = summarize_text(text, language, summary_size, summarizer_type)
         st.write(summary)
         st.write(f"Token size: {len(summary.split())}")          
 
 elif prompt_type == "Code":
     st.header("Code")
     code = st.text_area("Enter Python code")
-    if st.button("Clean the code"):
-        cleaned_code = remove_annotations(code)
-        st.code(cleaned_code)
-        st.write(f"Token size: {len(cleaned_code.split())}")
+    if st.button("Format the code"):
+        try:
+            formatted_code = black.format_str(code, mode=black.Mode())
+            st.code(formatted_code)
+            st.write(f"Token size: {len(formatted_code.split())}")
+        except Exception as e:
+            st.write(f"Error: {e}")

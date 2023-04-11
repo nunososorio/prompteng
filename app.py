a130b23ae36cfc09431ec0c11b4447ce89c9069b
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-11 02:11:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,11 @@ prompt_type = st.sidebar.selectbox("Select prompt type", ["Text", "Code"])
 
 if prompt_type == "Text":
     st.header("Text")
+    scaffolding_sentences = ["Perform sentiment analysis", "The main point of this text is", "The purpose of this research is to", "Generate a 100chr summary", "This topic is important because", "A common misconception related to this text is:", "The most significant factor in the text is", "One possible solution to this problem is", "The most important thing to remember about this text is", "Another way to think about this text is", "Rewrite for clarity", "The key takeaway from this article is", "Copy-edit", "One limitation of this study is that", "The implications of the text are", "This research contributes to our understanding of", "Proofread", "Revise", "Format", "Proofread, revise, copy-edit and format"]
+    selected_sentence = st.selectbox("Select scaffolding sentence", scaffolding_sentences)
+    
+    prompt_text = st.text_area("Formulate your prompt", value=selected_sentence)
+    
     languages = ["czech", "dutch", "english", "french", "german", "italian", "portuguese", "romanian", "russian", "slovak", "spanish"]
     language = st.selectbox("Select language", languages)
     text = st.text_area("Enter text to summarize")
@@ -48,8 +53,16 @@ if prompt_type == "Text":
         summary = summarize_text(text, language, summary_size, summarizer_type)
         st.write(summary)
         st.write(f"Token size: {len(summary.split())}")
+        
 elif prompt_type == "Code":
     st.header("Code")
+    
+    scaffolding_sentences = ["Let’s take a step back and think about what we want to achieve.", "What are the inputs we need to consider?", "What are the outputs we expect?", "How can I alter the code to help debugging?", "Let’s write some pseudocode to help us think through the problem.", "Add print statements to the code to track progress.", "Let’s start by writing some tests to make sure our code works.", "What are some potential issues with our code?", "How can we make our code more efficient?", "How can we make our code more readable?", "What are some ways we can improve our code?", "Let’s refactor our code to make it more modular.", "What are some ways we can optimize our code?", "Let’s add some error handling to our code.", "How can we make our code more maintainable?", "Does the code below follows best practices for writing clean code?", "Let’s review our code and make sure it meets our requirements."]
+    
+    selected_sentence = st.selectbox("Select scaffolding sentence", scaffolding_sentences)
+    
+    prompt_text = st.text_area("Formulate your prompt", value=selected_sentence)
+    
     code = st.text_area("Enter Python code")
     if st.button("Format the code"):
         try:
